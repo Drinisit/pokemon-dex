3972e808c8b867dfb868d7852ca2b0aa5c8d97c4
Drin <aldrinisit3196@gmail.com>
2024-03-06 20:36:54 +0800

    Add: dynamic description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-function Header(){
-  return(
+const reactDescriptions = [`Gotta Catch 'Em All!`, 'The adventure begins!', 'The ultimate Pokémon experience.','Become a Pokémon Master!'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(3)];
+  return (
     <header>
-    <img src="src/assets/pokemondex.webp" alt="" />
-    <h1>POKEMON</h1>
-    <p>Gatta Catch Em All</p>
-</header>
-  )
+      <img src="src/assets/pokemondex.webp" alt="" />
+      <h1>POKEMON</h1>
+      <p>{description}</p>
+    </header>
+  );
 }
 
 function App() {
