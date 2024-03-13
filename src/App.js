b0700b9f73059420ddd25aae3583e96b417c5657
Drin <aldrinisit3196@gmail.com>
2024-03-13 21:49:08 +0800

    Add: map to display each item

--- src/App.js
+++ src/App.js
@@ -38,15 +38,9 @@ function App() {
           <br />
           <h2>Pokedex</h2>
           <ul>
-            <PokeCard
-              name={POKEMON_LIST[0].name}
-              description={POKEMON_LIST[0].description}
-              image={POKEMON_LIST[0].image}
-            />
-
-            <PokeCard {...POKEMON_LIST[1]} />
-            <PokeCard {...POKEMON_LIST[2]} />
-            <PokeCard {...POKEMON_LIST[3]} />
+            {POKEMON_LIST.map((pokemonItem) => (
+              <PokeCard key={pokemonItem.name} {...pokemonItem} />
+            ))}
           </ul>
         </section>
         <section className="legendary">
